af6e9b5aacaf02917842215a3a854c7775df15f2
Damian <damianoven7@gmail.com>
2023-12-03 21:12:20 -0800

    Part 2 solution (not working due to nonoverlapping regexp behavior)

--- 2023/Day_01/src/main.rs
+++ 2023/Day_01/src/main.rs
@@ -2,6 +2,9 @@ use std::fs;
 use regex::Regex;
 
 fn main() {
+    // choose solution for part 1 or part 2
+    let part1: bool = false; // part 2
+
     // Read input data
     let contents: String = fs::read_to_string("input.txt")
         .expect("Error");
@@ -17,19 +20,36 @@ fn main() {
 
     // initialize regular expressions
     let single_digit_re: Regex = Regex::new("[0-9]{1}").unwrap();
+    let part_two_matches: Vec<_> = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", ""];
+    let part_two_re_text: String = part_two_matches.join("{1}|")
+        .strip_suffix("|").unwrap().to_string();
+    let complex_re: Regex = Regex::new(&part_two_re_text).unwrap();
+    let re: Regex = if part1 {single_digit_re} else {complex_re};
 
     // iterate over each line
     for line in lines {
-        // find first number in this line
-        let digit_hits: Vec<&str> = single_digit_re.find_iter(line)
+        // find hits in this line
+        let digit_hits: Vec<&str> = re.find_iter(line)
             .map(|m| m.as_str()).collect();
+        // get first digit
+        let first_digit: String = if digit_hits[0].to_string().len() > 1 {
+            // complex hit
+            part_two_matches[part_two_matches.iter()
+                .position(|&x| x==digit_hits[0].to_string())
+                .unwrap()-9].to_string()
+        } else {digit_hits[0].to_string()};
+        // get second digit
+        let second_digit: String = if digit_hits[digit_hits.len()-1].to_string().len() > 1 {
+            // complex hit
+            part_two_matches[part_two_matches.iter()
+                .position(|&x| x==digit_hits[digit_hits.len()-1].to_string())
+                .unwrap()-9].to_string()
+        } else {digit_hits[digit_hits.len()-1].to_string()};
         // create two digit number
-        let two_digit_number: Vec<_> = vec![digit_hits[0], digit_hits[digit_hits.len()-1]];
-        let two_digit_number: String = two_digit_number.concat();
-        let two_digit_number: u32 = two_digit_number.parse::<u32>().unwrap();
+        let two_digit_number: u32 = [first_digit, second_digit].concat().parse::<u32>().unwrap();
         // increment counter
         counter += two_digit_number;
-        println!("Int. result: {}", two_digit_number);
     }
 
     // display result
