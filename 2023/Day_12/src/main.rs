20d40b4004f38fdb64b61f313b5fa4428b015a7f
Damian <damianoven7@gmail.com>
2023-12-18 21:30:39 -0800

    part 2, but slow

--- 2023/Day_12/src/main.rs
+++ 2023/Day_12/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use regex::Regex;
 
 fn main() {
     // load input
@@ -7,68 +8,50 @@ fn main() {
     // Part 1, 2
     let mut num_total: usize = 0;
     for (i, entry) in input.iter().enumerate() {
+        // initialize valid case counter
         let mut valid_cases: usize = 0;
-        gen_cases(&mut valid_cases, &entry.0, &entry.1);
+        // create regex for this group pattern
+        let group_re = build_regex(&entry.1);
+        gen_cases(&mut valid_cases, &entry.0, &entry.1, &group_re);
         num_total += valid_cases;
         println!("[Case {:04}] Valid: {}", i, valid_cases);
     }
     println!("Part 1: {}", num_total);
 }
 
-fn gen_cases(valid_cases: &mut usize, current_case: &String, group_sizes: &Vec<usize>) {
+fn build_regex(group_sizes: &Vec<usize>) -> Regex {
+    let group_matches: Vec<String> = group_sizes.iter()
+        .map(|&x| format!("([#|?]{{{}}}[O|?]{{1}})", x))
+        .collect();
+    let misc_match: String = "([O?]*)".to_string();
+    let full_match = "(^[O?]*)".to_owned() + &group_matches.join(&misc_match);
+    Regex::new(&full_match).unwrap()
+}
+
+fn gen_cases(valid_cases: &mut usize, current_case: &String, group_sizes: &Vec<usize>, group_re: &Regex) {
     // check if this case is impossible
-    if is_impossible(current_case, group_sizes) {
-        // do nothing and return... this branch is dead
+    if !group_re.is_match(&current_case) {
+        // this does not work
         return;
     }
     // check if this is the base case
     if !current_case.contains("?") {
         // base case!
-        // increment the number of cases
-        *valid_cases += 1;
-        println!("{}", valid_cases);
+        // check that this is a valid solution
+        if current_case.chars().filter(|&x| x=='#').count() == group_sizes.iter().sum() {
+            // increment the number of cases
+            *valid_cases += 1;
+        }
         return;
     }
     // recurse assuming the first '?' is either '.' or '#'
     gen_cases(valid_cases,
-        &current_case.replacen("?", ".", 1),
-        group_sizes);
+        &current_case.replacen("?", "O", 1),
+        group_sizes, group_re);
     gen_cases(valid_cases,
         &current_case.replacen("?", "#", 1),
-        group_sizes);
+        group_sizes, group_re);
     return;
-}
-
-fn is_impossible(case: &String, group_sizes: &Vec<usize>) -> bool {
-    // returns T/F for if the given list and group sizes are incompatible.
-
-    // append '.' to the list of chars
-    let mut case_chars: Vec<char> = case.chars().collect();
-    case_chars.push('.');
-    // try to fit in each group
-    let mut idx_search: usize = 0;
-    for i in 0..group_sizes.len() {
-        // try each index
-        let idx_start = (idx_search..case.len()-group_sizes[i]+1)
-            .find(|&f| (0..group_sizes[i]+1)
-                .all(|x| if x < group_sizes[i] {
-                    case_chars[f+x]=='?' || case_chars[f+x]=='#'
-                } else {
-                    case_chars[f+x]=='?' || case_chars[f+x]=='.'
-                }));
-        if idx_start.is_none() {
-            // could not find a place for this group
-            return true;
-        } else {
-            // "insert" this group
-            idx_search = idx_start.unwrap() + group_sizes[i] - 1 + 1;
-        }
-    }
-    // check that the number of '#' matches the group sum
-    if !case.contains("?") && (case.chars().filter(|&x| x=='#').count() != group_sizes.iter().sum()) {
-        return true;
-    }
-    return false;
 }
 
 fn load_input(file_name: &str, part1: bool) -> Vec<(String, Vec<usize>)> {
@@ -80,7 +63,8 @@ fn load_input(file_name: &str, part1: bool) -> Vec<(String, Vec<usize>)> {
             x.split_whitespace()
                 .nth(0)
                 .unwrap()
-                .to_string(),
+                .to_string()
+                .replace('.', "O"),
             x.split_whitespace()
                 .nth(1)
                 .unwrap()
@@ -89,14 +73,16 @@ fn load_input(file_name: &str, part1: bool) -> Vec<(String, Vec<usize>)> {
                 .collect::<Vec<usize>>()
                 .repeat(if part1 {1} else {5})))
         .collect();
-    if !part1 {
         for i in 0..input.len() {
-            let mut temp_string: String = input[i].0.clone();
-            temp_string.push_str("?");
-            temp_string = temp_string.repeat(5);
-            temp_string.remove(temp_string.len()-1);
-            input[i].0 = temp_string;
+            if part1 {
+                input[i].0 = input[i].0.clone() + "O";
+            } else {
+                let mut temp_string: String = input[i].0.clone();
+                temp_string.push_str("?");
+                temp_string = temp_string.repeat(5);
+                temp_string.remove(temp_string.len()-1);
+                input[i].0 = temp_string + "O";
+            }
         }
-    }
     input
 }
