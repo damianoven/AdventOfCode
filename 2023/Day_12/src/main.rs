07d6bfcb110042d72eaa1ac400bcee4a18ad14bd
Damian <damianoven7@gmail.com>
2023-12-20 17:27:38 -0800

    Solution caching to make it faster

--- 2023/Day_12/src/main.rs
+++ 2023/Day_12/src/main.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use std::{fs, collections::hash_map};
 use regex::Regex;
+use std::collections::HashMap;
 
 fn main() {
     // load input
@@ -8,23 +9,16 @@ fn main() {
     // Part 1, 2
     let mut num_total: usize = 0;
     for (i, entry) in input.iter().enumerate() {
-        num_total += num_valid(&entry.0, 0, entry.1.clone());
+        let mut cache: HashMap<String, usize> = HashMap::new();
+        num_total += num_valid(&entry.0, 0, entry.1.clone(), &mut cache);
         println!("[Case {:04}] done", i);
     }
     println!("Part 1: {}", num_total);
 }
 
-fn num_valid(case: &String, start_idx: usize, group_sizes: Vec<usize>) -> usize {
+fn num_valid(case: &String, start_idx: usize, group_sizes: Vec<usize>, solution_cache: &mut HashMap<String, usize>) -> usize {
     // constants
     let hash: String = "#".to_string();
-    // min number of entries to satisfy groups
-    let min_len: usize = group_sizes.iter().sum::<usize>() + group_sizes.len();
-    // characters available
-    let chars_available: usize = case.len() - start_idx;
-    // if there isn't room, then return 0
-    if min_len > chars_available {
-        return 0;
-    }
     // check if base case
     if group_sizes.len() == 1 {
         // calculate how many ways the entry can fit in the remaining characters
@@ -35,6 +29,8 @@ fn num_valid(case: &String, start_idx: usize, group_sizes: Vec<usize>) -> usize 
                 !case[x..x+group_sizes[0]].contains("O") && 
                 case[x+group_sizes[0]..x+group_sizes[0]+1]!=hash).count();
     } else {
+        // min number of entries to satisfy groups
+        let min_len: usize = group_sizes.iter().sum::<usize>() + group_sizes.len();
         // adjust the first group from the first position to the last available position
         let mut running_total: usize = 0;
         for i in start_idx..case.len()-min_len+1 {
@@ -46,49 +42,24 @@ fn num_valid(case: &String, start_idx: usize, group_sizes: Vec<usize>) -> usize 
             if !case[i..i+group_sizes[0]].contains("O") && 
                 case[i+group_sizes[0]..i+group_sizes[0]+1]!=hash && 1==1 {
                 // this is valid, leave the first group here and recurse on remaining groups
-                running_total += num_valid(case, i+group_sizes[0]+1, group_sizes[1..].to_vec());
+                let hash_key: String = format!("{}{}{}_#$k{}{}{}",
+                case, i+group_sizes[0]+1, group_sizes.len()-1, i+group_sizes[0]+1, case, group_sizes.len()-1);
+                let mut remaining_valid: usize = 0;
+                if solution_cache.contains_key(&hash_key) {
+                    // we've computed this before... just grab the result
+                    remaining_valid = *solution_cache.get(&hash_key).unwrap();
+                } else {
+                    // need to compute this for the first time
+                    remaining_valid = num_valid(case, i+group_sizes[0]+1,
+                        group_sizes[1..].to_vec(), solution_cache);
+                    // add to map
+                    solution_cache.insert(hash_key, remaining_valid);
+                }
+                running_total += remaining_valid;
             }
         }
         return running_total;
     }
-}
-
-
-
-fn build_regex(group_sizes: &Vec<usize>) -> Regex {
-    let group_matches: Vec<String> = group_sizes.iter()
-        .map(|&x| format!("([#|?]{{{}}}[O|?]{{1}})", x))
-        .collect();
-    let misc_match: String = "([O?]*)".to_string();
-    let full_match = "(^[O?]*)".to_owned() + &group_matches.join(&misc_match);
-    Regex::new(&full_match).unwrap()
-}
-
-fn gen_cases(valid_cases: &mut usize, current_case: &String, group_sizes: &Vec<usize>, group_re: &Regex) {
-    // check if 
-    // check if this case is impossible
-    if !group_re.is_match(&current_case) {
-        // this does not work
-        return;
-    }
-    // check if this is the base case
-    if !current_case.contains("?") {
-        // base case!
-        // check that this is a valid solution
-        if current_case.chars().filter(|&x| x=='#').count() == group_sizes.iter().sum() {
-            // increment the number of cases
-            *valid_cases += 1;
-        }
-        return;
-    }
-    // recurse assuming the first '?' is either '.' or '#'
-    gen_cases(valid_cases,
-        &current_case.replacen("?", "O", 1),
-        group_sizes, group_re);
-    gen_cases(valid_cases,
-        &current_case.replacen("?", "#", 1),
-        group_sizes, group_re);
-    return;
 }
 
 fn load_input(file_name: &str, part1: bool) -> Vec<(String, Vec<usize>)> {
