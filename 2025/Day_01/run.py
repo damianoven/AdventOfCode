97851ea9e3f9098021809f07e2728d1ce8aca173
Damian <damianoven7@gmail.com>
2025-12-01 18:24:47 -0800

    day 1 part 2

--- 2025/Day_01/run.py
+++ 2025/Day_01/run.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # read file
 f = open('input.txt','r');
@@ -6,11 +7,29 @@ rawLines = [x.strip() for x in f];
 f.close();
 
 # parse lines
-turnAmount = [(x.startswith('R')*2-1)*int(x[1:]) for x in rawLines];
+turnAmount = np.array([(x.startswith('R')*2-1)*int(x[1:]) for x in rawLines]);
+
+# start at 50
+turnAmount = np.insert(turnAmount,0,50);
 
 # cumulative sum, starting at 50
-cSum = 50 + np.cumsum(turnAmount);
+cSum = np.cumsum(turnAmount);
 
 # count when sum mod 100 == 0
-numZeros = sum((cSum % 100) == 0);
-print(numZeros)
+exactZero = (cSum % 100) == 0;
+print(sum(exactZero))
+
+# count wrap-around 0s
+wrapZeros = np.floor(np.abs(turnAmount)/100);
+turnAmountUnambiguous = turnAmount - wrapZeros*100*np.sign(turnAmount); # all turns are 0-99 now
+
+# if diff between the two values mod 100 doesn't equal diff mod 100, a wrap occured
+# exclude turns to or from 0, which cause a double-count half of the time
+modDiff = ((cSum[1:] % 100) - (cSum[:-1] % 100));
+diffMod = turnAmountUnambiguous[1:]
+diffWrap = modDiff != diffMod;
+diffWrap = np.logical_and(diffWrap, np.invert(exactZero[1:])); # moving to zero
+diffWrap = np.logical_and(diffWrap, np.invert(exactZero[:-1])); # moving from zero
+
+# final count
+print(sum(exactZero) + sum(wrapZeros) + sum(diffWrap));
