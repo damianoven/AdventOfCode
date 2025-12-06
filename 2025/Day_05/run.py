7249854be0a748006e5de5149c17a4208ccb77a7
Damian <damianoven7@gmail.com>
2025-12-06 09:20:48 -0800

    Logic to remove eclipsed ranges

--- 2025/Day_05/run.py
+++ 2025/Day_05/run.py
@@ -21,13 +21,22 @@ print(sum(np.any(is_in_range(validRanges[:,0],validRanges[:,1],testValues),0)))
 
 # part 2
 
+# if any range is completely contained inside another range,
+# remove it from the list of ranges
+startContained = is_in_range(validRanges[:,0],validRanges[:,1],validRanges[:,0])*1-np.identity(len(validRanges))
+stopContained = is_in_range(validRanges[:,0],validRanges[:,1],validRanges[:,1])*1-np.identity(len(validRanges))
+print(startContained)
+print(startContained.astype('bool'))
+idxRemove = np.any(startContained.astype('bool') & stopContained.astype('bool'),0)
+validRanges = np.delete(validRanges,idxRemove,0)
+
 # if any starting value is itself within a range, move it
 # to the end of that range +1
 startIsInRange = is_in_range(validRanges[:,0],validRanges[:,1],validRanges[:,0])
-newStartingValue = startIsInRange * (np.reshape(validRanges[:,1],(-2,1))+1) 
+newStartingValue = startIsInRange * (np.reshape(validRanges[:,1],(-2,1))+1)
 newStartingValue -= newStartingValue * np.identity(len(newStartingValue)).astype('longlong') # remove diagonal (self range)
 newStartingValue = np.max(newStartingValue,0)
 validRanges[newStartingValue>0,0] = newStartingValue[newStartingValue>0];
-
 # add up ranges as ints
-print(sum([int(validRanges[x,1])-int(validRanges[x,0])+1 for x in range(len(validRanges))]))
+rangeSums = [int(validRanges[x,1])-int(validRanges[x,0])+1 for x in range(len(validRanges))]
+print(sum([(x>0)*x for x in rangeSums]))
